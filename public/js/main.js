38806c6549e8de6c5fd4c50870f595e4c7f37e28
natchez <natchezzz@hotmail.com>
2021-02-09 00:17:23 +0100

    fin section1

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,20 @@ let close = document.querySelector(`.close`)
 close.addEventListener('click', () => {
     nav.style.width = "0%"
 })
+
+
+
+
+// boucler  l'anim des produits
+for (let i = 0; i < 9; i++) {
+    let card = document.querySelectorAll('.view')[i]
+    let image = document.querySelectorAll('.produit_calque')[i]
+    image.addEventListener('mouseenter', () => {
+        card.style.height = "100%"
+        card.style.bottom ="60px"
+    })
+    image.addEventListener('mouseout', () => {
+        card.style.height = "0%"
+        card.style.bottom ="0px"
+    })
+}
