4cbe8a5207b66eac69908526bc798c8cff2fd363
natchez <natchezzz@hotmail.com>
2021-02-08 19:21:17 +0100

    fin banner et responsive header

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
+// selection du menu déroulant 
+let nav = document.querySelector(`#navburger`)
 
+// ouvrir le menu
+let open = document.querySelector(`#burger-icon`)
+open.addEventListener('click', () => {
+    nav.style.width = "100%"
+})
+
+
+// fermer le menu
+let close = document.querySelector(`.close`)
+close.addEventListener('click', () => {
+    nav.style.width = "0%"
+})
